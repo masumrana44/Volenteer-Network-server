d90b4e0347bd43383f14992dd9f44187800cbaeb
Masum Rana <masumrana446267@gmail.com>
2023-04-18 10:54:28 +0600

    JWT security features added

--- index.js
+++ index.js
@@ -2,13 +2,50 @@ const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { decode } = require("punycode");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
 app.use(express.json());
+
+// jwt Verification Middleware
+// const verifyJWT = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ meassage: "unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+//     if(err){
+//       return res.status(401).send({message:"unauthorized access"});
+//     }
+//        req.decoded=decoded;
+//  return next()
+//   })
+
+// };
+
+function verifyJWT(req, res, next) {
+  const authHeader=req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message:'unauthorized access'})
+  }
+
+  const token=authHeader.split(' ')[1];
+   jwt.verify(token,process.env.ACCESS_TOKEN,function(err,decoded){
+    if(err){
+      return res.status(401).send({message:'unauthorized access'})
+    }
+    req.decoded=decoded;
+     next()
+   })
+
+  // console.log(authHeader)
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w9y9xep.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,6 +65,14 @@ const run = async () => {
       .db("Donation-Collection")
       .collection("DonationStory");
 
+    // for wtw token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
     // Posting Data  Mongodb server form Client site
     app.post("/add/event", async (req, res) => {
       const event = req.body;
@@ -59,28 +104,29 @@ const run = async () => {
       res.send(event);
     });
 
-    app.get("/donation", async (req, res) => {
+    app.get("/donation",verifyJWT, async (req, res) => {
       const email = req.query.email;
-      console.log(email)
+      const decoded=req.decoded;
+      if(decoded.email !==email){
+        req.status(403).send({message:'unauthorized access'})
+      } 
       let query = {};
       if (req.query) {
         query = {
           email: email,
         };
       }
-      const cursor =  donationCollection.find(query);
+      const cursor = donationCollection.find(query);
       const events = await cursor.toArray();
       res.send(events);
-
     });
 
-    app.delete('/delete/donation/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)};
-      const result=await donationCollection.deleteOne(query);
-      res.send(result)
-      
-    })
+    app.delete("/delete/donation/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationCollection.deleteOne(query);
+      res.send(result);
+    });
   } catch {
   } finally {
   }
